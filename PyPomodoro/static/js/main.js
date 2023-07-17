8f12b577b12c0ecd4a133ea2f68dafbb9d54d2f1
DrKakku <DrKakku19@gmail.com>
2023-07-17 09:45:27 +0530

    Timer working

--- PyPomodoro/static/js/main.js
+++ PyPomodoro/static/js/main.js
@@ -6,8 +6,24 @@ function Start() {
 
 function Pause() {
     console.log("Pause");
+    var now = new Date() //.toString()
+    clock.innerHTML = now.toLocaleTimeString()
 }
 
 function Stop() {
     console.log("Stop");
 }
+var clock = document.getElementsByClassName("clock")[0]
+
+function setTime ()
+{
+    var now = new Date()
+    clock.innerHTML = now.toLocaleTimeString()
+    console.log("running")
+}
+
+let timer = setInterval(
+    setTime,500
+);
+
+
